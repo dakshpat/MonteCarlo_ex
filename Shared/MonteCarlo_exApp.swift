716115dad1c0dcb62b0dfdb8887e62a658fe625c
Daksh <dpatel132@hawk.iit.edu>
2022-02-04 12:57:21 -0600

    Initial Commit

---
+++ Shared/MonteCarlo_exApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonteCarlo_exApp.swift
+//  Shared
+//
+//  Created by Daksh Patel on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MonteCarlo_exApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
